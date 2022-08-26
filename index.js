a1d7a7c913191ab7d1634fbef3a0803a135cc98c
VanLuarK <vanluark99@gmail.com>
2022-08-26 14:13:30 +0200

    Adding variables to query

--- index.js
+++ index.js
@@ -19,15 +19,15 @@ const typeDefs = gql`
   }
 
   type Query {
-    getNews: [Article]!
+    getNews(topic: String!, date: String!, language: String!): [Article]!
   }
 `;
 
 const resolvers = {
   Query: {
-    getNews: async () => {
+    getNews: async (root, args) => {
       const { data } = await axios.get(
-        "https://newsapi.org/v2/everything?q=tesla&from=2022-07-26&sortBy=publishedAt&apiKey=43c6c8478e6e45cb85229870acb08904"
+        `https://newsapi.org/v2/everything?q=${args.topic}&from=${args.date}&sortBy=publishedAt&language=${args.language}&apiKey=43c6c8478e6e45cb85229870acb08904`
       );
       return data.articles.filter((article) => article.urlToImage);
     },
